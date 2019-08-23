1439d83a93970e28ae077eb1df010421a16e1695
Sonia <sonia@gmail.com>
2019-08-23 10:07:03 +0100

    testing ip

--- app/src/main/java/retrofit/example/coinmarket/Client.kt
+++ app/src/main/java/retrofit/example/coinmarket/Client.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Client {
-    val BASE_URL = "https://api.coinmarketcap.com/v1/"
+    val BASE_URL = "https://104.17.140.178/v1/"
     var retrofit: Retrofit? = null
 
     fun getClient(): Retrofit {
